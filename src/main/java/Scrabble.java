9b4e4a677aaf8cd9492c070b89d36e8a9798ccb9
Colin Bloom <colinjbloom113@gmail.com>
2017-03-21 13:57:24 -0700

    add functionality for calculateScore of single letter

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 public class Scrabble {
   public Integer calculateScore(String input) {
     Integer result = 0;
+
+    if ( input.equals("a") ) {
+      result += 1;
+    }
     return result;
   }
 }
