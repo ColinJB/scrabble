6f15dc0efce7b9ea56502238bd03e7c93b14c20a
Colin Bloom <colinjbloom113@gmail.com>
2017-03-21 13:46:30 -0700

    initial commit, create Scrabble.java, App.java, ScrabbleTest.java

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+import java.util.List;
+import java.util.ArrayList;
+
+public class App {
+  public static void main (String[] args) {
+    System.out.println("Scrabble son!");
+  }
+}
---
+++ src/main/java/Scrabble.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Scrabble {
+  public Integer calculateScore(String input) {
+    Integer result = 0;
+    return result;
+  }
+}
